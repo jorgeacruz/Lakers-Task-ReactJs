e78a0dca1b1909b0e68baea8decbd0cd0fa2be9d
Jorge.acruz <jorge.cruz@crmservives.com.br>
2022-11-17 13:26:35 -0300

    Login system with ReactJs

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,33 @@
+import { useState } from "react"
+
 export default function Home(){
+
+  const [email,setEmail] = useState();
+  const [password, setPassword] = useState();
+
     return(
       <div>
-        <h1>Página Homesas</h1>
+        <h1>Lista de tarefas</h1>
+        <span>Gerencie sua agenda de forma fácil</span>
+
+        <form className="form">
+          
+          <input
+          autoComplete={false}
+          type="text"
+          placeholder="Digite seu email..."
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}/>
+          
+          <input
+          type="password"
+          placeholder="********"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}/>
+
+          <button type="submit"></button>
+
+        </form>
       </div>
     )
   }
