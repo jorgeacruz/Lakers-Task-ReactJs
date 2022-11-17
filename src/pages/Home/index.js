0fb6f766466d993eeabc2c0b2197a4954d77b6a3
Jorge.acruz <jorge.cruz@crmservives.com.br>
2022-11-17 16:24:22 -0300

    Update files

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,33 +1,50 @@
 import { useState } from "react"
+import { Link } from 'react-router-dom';
 
-export default function Home(){
+import './home.css';
 
-  const [email,setEmail] = useState();
+export default function Home() {
+
+  const [email, setEmail] = useState();
   const [password, setPassword] = useState();
 
-    return(
-      <div>
-        <h1>Lista de tarefas</h1>
-        <span>Gerencie sua agenda de forma fácil</span>
+  function handleLogin(e) {
+    // Event para nâo recarregar o form
+    e.preventDefault();
+    // condicional
+    if( email !== '' && password !== ''){
+      alert('teste');
+    } else {
+      alert('Preencha os campos vazios');
+    }
+  }
 
-        <form className="form">
-          
-          <input
+  return (
+    <div className="home-container">
+      <img src='https://1000logos.net/wp-content/uploads/2017/03/los-angeles-lakers-logo.jpg' alt="" className="img-logo" />
+      <h1>Clube Lakers Advanced</h1>
+      <span>Gerencie sua agenda de forma fácil</span>
+
+      <form className="form" onSubmit={handleLogin}>
+
+        <input
           autoComplete={false}
           type="text"
           placeholder="Digite seu email..."
           value={email}
-          onChange={(e) => setEmail(e.target.value)}/>
-          
-          <input
+          onChange={(e) => setEmail(e.target.value)} />
+
+        <input
           type="password"
           placeholder="********"
           value={password}
-          onChange={(e) => setPassword(e.target.value)}/>
+          onChange={(e) => setPassword(e.target.value)} />
 
-          <button type="submit"></button>
+        <button type="submit" className="btn-submit">Acessar</button>
+        <Link to="/register" className="button-link">Não possuo uma conta? Cadastra-se.</Link>
 
-        </form>
-      </div>
-    )
-  }
+      </form>
+
+    </div>
+  )
+}
